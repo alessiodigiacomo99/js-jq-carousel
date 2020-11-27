561f2eb6acc081814fc0be647755a23b688543ef
alessio <alessiodg99.adg@gmail.com>
2020-11-27 19:02:38 +0100

    se clicco sul bottone cambio bottone e immagine

--- bonus.js
+++ bonus.js
@@ -1,10 +1,14 @@
 var frecciaSinistra = $("div.prev");
 var frecciaDestra = $("div.next");
-var imgSuccessiva, imgCorrente, imgPrecedente, bottoneSuccessivo, bottonePrecedente;
+var imgSuccessiva, corrente, imgPrecedente, bottoneSuccessivo, bottonePrecedente;
 var ultimo = $(".last");
 var primo = $(".first");
-var secondaImg = $("img:nth-child(2)")
-var terzaImg = $("img:nth-child(3)")
+var secondaImg = $("img:nth-child(2)");
+var terzaImg = $("img:nth-child(3)");
+var primoBottone = $("i.first")
+var secondoBottone = $("i:nth-child(2)");
+var terzoBottone = $("i:nth-child(3)");
+var quartoBottone = $("i.last");
 frecciaDestra.click(function(){
     corrente = $(".active");
     imgSuccessiva = $(".active + img");
@@ -34,4 +38,26 @@ frecciaSinistra.click(function(){
     corrente.removeClass("active");
     imgPrecedente.addClass("active");
     bottonePrecedente.addClass("active");
+})
+primoBottone.click(function(){
+    corrente = $(".active");
+    corrente.removeClass("active");
+    primo.addClass("active");
+})
+secondoBottone.click(function(){
+    corrente = $(".active");
+    corrente.removeClass("active");
+    secondoBottone.addClass("active");
+    secondaImg.addClass("active");
+})
+terzoBottone.click(function(){
+    corrente = $(".active");
+    corrente.removeClass("active");
+    terzoBottone.addClass("active");
+    terzaImg.addClass("active");
+})
+quartoBottone.click(function(){
+    corrente = $(".active");
+    corrente.removeClass("active");
+    ultimo.addClass("active");
 })
